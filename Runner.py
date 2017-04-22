69211f97c24a4a783aa912239b2275ae7814f8c7
Suyog Soti <suyog.soti@gmail.com>
2017-04-22 17:30:58 -0600

    switch to selenium

--- Runner.py
+++ Runner.py
@@ -1,21 +1,19 @@
-from Tkinter import Tk
 import os
-from tkFileDialog import askopenfilename
-import Image
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
 
 
 def main():
-    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-    if(os.path.exists(filename) == False):
-        print("The image no longer exists...Please enter a new file")
-        return
-    try:
-        im = Image.open(filename)
-    except IOError:
-        print ("The file is either not an image file or the file is currupted!")
+    display = Display(visible=0, size=(1080, 1920))
+    display.start()
+    driver = webdriver.Chrome()
+    driver.get("https://earth.google.com/web/@40.00122818,-105.26285119,1644.76720416a,73.34143392d,35y,89.6767488h,66.20806007t,-0r/data=CksaSRJDCiUweDg3NDAxNDc0OWIxODU2Yjc6MHhjNzU0ODMzMTQ5OTBhN2ZmGY1QB5lipkNAIYAWaIEWYFrAKghDT0xPUkFETxgBIAE")
+    # print driver.page_source.encode('utf-8')
+    driver.save_screenshot("google.png")
+    driver.quit()
+    display.stop()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
