032f24a39abe86763efdad39f9a340c7a3b745b4
Suyog Soti <suyog.soti@gmail.com>
2017-04-22 20:06:27 -0600

    web native chrome webbrowser instead of selenium

--- Runner.py
+++ Runner.py
@@ -1,18 +1,11 @@
-import os
-from selenium import webdriver
-from pyvirtualdisplay import Display
+import webbrowser
 
-
+# def open_coordinates(long, latd, lat):
+#     return "https://earth.google.com/web/@40.00122199,-105.26285016,1650.67707633a,54.79800433d,35y,88.6646013h,68.36048567t,-0r/data=CksaSRJDCiUweDg3NDAxNDc0OWIxODU2Yjc6MHhjNzU0ODMzMTQ5OTBhN2ZmGY1QB5lipkNAIYAWaIEWYFrAKghDT0xPUkFETxgBIAE"
 
 def main():
-    display = Display(visible=0, size=(1080, 1920))
-    display.start()
-    driver = webdriver.Chrome()
-    driver.get("https://earth.google.com/web/@40.00122818,-105.26285119,1644.76720416a,73.34143392d,35y,89.6767488h,66.20806007t,-0r/data=CksaSRJDCiUweDg3NDAxNDc0OWIxODU2Yjc6MHhjNzU0ODMzMTQ5OTBhN2ZmGY1QB5lipkNAIYAWaIEWYFrAKghDT0xPUkFETxgBIAE")
-    # print driver.page_source.encode('utf-8')
-    driver.save_screenshot("google.png")
-    driver.quit()
-    display.stop()
+    webbrowser.open("https://earth.google.com/web/@40.00122199,-105.26285016,1650.67707633a,54.79800433d,35y,88.6646013h,68.36048567t,-0r/data=CksaSRJDCiUweDg3NDAxNDc0OWIxODU2Yjc6MHhjNzU0ODMzMTQ5OTBhN2ZmGY1QB5lipkNAIYAWaIEWYFrAKghDT0xPUkFETxgBIAE")
+    webbrowser.get("http://google.com")
 
 
 if __name__ == "__main__":
